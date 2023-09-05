b3043d832b2b2a35a00ae49e6c24e163dc4ff85d
anrajin <an.rajin@gmail.com>
2023-09-05 23:54:08 +0600

    sign in page design added

--- event-bud-frontend/src/app/app-routing.module.ts
+++ event-bud-frontend/src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { AdminComponent } from './admin/admin.component';
 import { AppComponent } from './app.component';
 import { AppRoutes } from './app.routes';
 import { PageNotFoundComponent } from './public/page-not-found/page-not-found.component';
+import { SigninComponent } from './public/signin/signin.component';
 
 const routes: Routes = [
   {
@@ -14,6 +15,10 @@ const routes: Routes = [
     path: AppRoutes.Admin,
     component: AdminComponent,
     loadChildren: () => import('./admin/admin.module').then((m) => m.AdminModule),
+  },
+  {
+    path: AppRoutes.Signin,
+    component: SigninComponent
   },
   {
     path: '**',
--- event-bud-frontend/src/app/public/public.module.ts
+++ event-bud-frontend/src/app/public/public.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PublicComponent } from './public.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { SigninComponent } from './signin/signin.component';
 
 
 
 @NgModule({
   declarations: [
     PublicComponent,
-    PageNotFoundComponent
+    PageNotFoundComponent,
+    SigninComponent
   ],
   imports: [
     CommonModule
