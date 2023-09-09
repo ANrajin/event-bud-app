0f54ae7eef095d2edeb49f8fc6fa8e7fc62ab602
anrajin <an.rajin@gmail.com>
2023-09-09 22:59:49 +0600

    created CategoriesController

--- event-bud-backend/src/presentations/EventBud.Api/Controllers/ApiBaseController.cs
+++ event-bud-backend/src/presentations/EventBud.Api/Controllers/ApiBaseController.cs
@@ -3,7 +3,6 @@
 namespace EventBud.Api.Controllers;
 
 [ApiController]
-[Route("api/v1/[controller]")]
 [Produces("application/json")]
 [Consumes("application/json")]
 public class ApiBaseController : Controller
---
+++ event-bud-backend/src/presentations/EventBud.Api/Controllers/v1/CategoriesController.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EventBud.Api.Controllers.v1;
+
+[Route("api/v1/[controller]")]
+public class CategoriesController : ApiBaseController
+{
+}
