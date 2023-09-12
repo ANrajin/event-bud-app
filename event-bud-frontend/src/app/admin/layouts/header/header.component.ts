db9383dedf8d0be33105c83b2a2f62235eb4c9a3
anrajin <an.rajin@gmail.com>
2023-09-12 23:02:25 +0600

    update: header component design and profile dropdown menu design

--- event-bud-frontend/src/app/admin/layouts/header/header.component.ts
+++ event-bud-frontend/src/app/admin/layouts/header/header.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+  isOpen: boolean = false;
+
+  onClickProfile = () => {
+    this.isOpen = !this.isOpen;
+  }
 
 }
