00d55e085cc088c66941bc2d02638eb487e9b5f7
anrajin <an.rajin@gmail.com>
2023-09-12 21:09:18 +0600

    new: header component added

--- event-bud-frontend/src/app/admin/layouts/layouts.module.ts
+++ event-bud-frontend/src/app/admin/layouts/layouts.module.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 
 
@@ -9,13 +10,15 @@ import { SidebarComponent } from './sidebar/sidebar.component';
 @NgModule({
   declarations: [
     SidebarComponent,
-    FooterComponent
+    FooterComponent,
+    HeaderComponent
   ],
   imports: [
     CommonModule,
     RouterModule
   ],
   exports: [
+    HeaderComponent,
     SidebarComponent,
     FooterComponent
   ]
