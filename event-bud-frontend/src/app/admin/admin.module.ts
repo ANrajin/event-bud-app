858d5dcffd89f771a73a7d0bf5c0fe922c3845b7
anrajin <an.rajin@gmail.com>
2023-09-14 23:21:46 +0600

    [new]: elements page component created

--- event-bud-frontend/src/app/admin/admin.module.ts
+++ event-bud-frontend/src/app/admin/admin.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AdminRoutingModule } from './admin-routing.module';
 import { LayoutsModule } from './layouts/layouts.module';
 
+import { ModalModule } from '../shared/modal/modal.module';
 import { AdminComponent } from './admin.component';
 import { AdminPageNotFoundComponent } from './views/admin-page-not-found/admin-page-not-found.component';
 import { DashboardComponent } from './views/dashboard/dashboard.component';
-import { SettingsModule } from './views/settings/settings.module';
 import { EventsComponent } from './views/events/events.component';
+import { SettingsModule } from './views/settings/settings.module';
+import { ElementsComponent } from './views/elements/elements.component';
 
 
 @NgModule({
@@ -16,13 +18,15 @@ import { EventsComponent } from './views/events/events.component';
     AdminComponent,
     DashboardComponent,
     AdminPageNotFoundComponent,
-    EventsComponent
+    EventsComponent,
+    ElementsComponent
   ],
   imports: [
     CommonModule,
     AdminRoutingModule,
     LayoutsModule,
-    SettingsModule
+    SettingsModule,
+    ModalModule
   ]
 })
 export class AdminModule { }
--- event-bud-frontend/src/app/admin/admin.routes.ts
+++ event-bud-frontend/src/app/admin/admin.routes.ts
@@ -2,6 +2,7 @@ export enum AdminRoutes{
     Dashboard = 'dashboard',
     Events = 'events',
     Settings = 'settings',
+    Elements = 'elements'
 }
 
 export enum SettingRoutes{
