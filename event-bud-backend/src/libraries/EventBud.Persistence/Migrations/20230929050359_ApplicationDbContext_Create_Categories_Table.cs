6dd63d8c931d54e14078a005bde839654aa155c4
anrajin <an.rajin@gmail.com>
2023-09-29 11:17:04 +0600

    [update] - migrations added

---
+++ event-bud-backend/src/libraries/EventBud.Persistence/Migrations/20230929050359_ApplicationDbContext_Create_Categories_Table.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EventBud.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class ApplicationDbContext_Create_Categories_Table : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "Application");
+
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                schema: "Application",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Categories",
+                schema: "Application");
+        }
+    }
+}
