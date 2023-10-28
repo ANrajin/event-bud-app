002f67b9da07e601eaccc361ff93521c8f0a70de
anrajin <an.rajin@gmail.com>
2023-10-28 18:14:12 +0600

    [update] - separate auth files

---
+++ event-bud-backend/src/api/EventBud.Api/Controllers/v1/IAM/SigninController.cs
@@ -0,0 +1,22 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using EventBud.Application.Features.Auth.Queries.SignIn;
+
+namespace EventBud.Api.Controllers.v1.IAM;
+
+public class SigninController : ApiBaseController
+{
+    public SigninController(ISender sender) 
+        : base(sender)
+    {
+    }
+    
+    [HttpPost]
+    public async Task<IActionResult> SignIn(
+        [FromBody] SignInQuery query, CancellationToken cancellationToken)
+    {
+        var result = await Sender.Send(query, cancellationToken);
+
+        return result.Succeeded ? Ok(result.Data) : BadRequest(result.Errors);
+    }
+}
---
+++ event-bud-backend/src/api/EventBud.Api/Controllers/v1/IAM/SignupController.cs
@@ -0,0 +1,22 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using EventBud.Application.Features.Auth.Commands.SignUp;
+
+namespace EventBud.Api.Controllers.v1.IAM;
+
+public class SignupController : ApiBaseController
+{
+    public SignupController(ISender sender) 
+        : base(sender)
+    {
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Post(
+        [FromBody] SignUpCommand command, CancellationToken cancellationToken)
+    {
+        var result = await Sender.Send(command, cancellationToken);
+        
+        return result.Succeeded ? Ok(result.Data) : BadRequest(result.Errors);
+    }
+}
